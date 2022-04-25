8588f335257f3ec2a749b90e454b01a3528cd01a
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-25 19:20:26 -0300

    Adding Text
    Adding some messages to the app

--- app/src/main/java/com/example/composebegins/MainActivity.kt
+++ app/src/main/java/com/example/composebegins/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.composebegins
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -13,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBeginsTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                MessageCard(Message("Colleague", "Jetpack Compose"))
             }
         }
     }
@@ -41,41 +45,53 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .border(0.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        Column (modifier = Modifier
+            .border(0.5.dp, Color.DarkGray, RectangleShape)
+            .padding(all = 8.dp)){
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
+                style = MaterialTheme.typography.subtitle2,
             )
 
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
             Text(
                 text = msg.body,
-                color = Color.Cyan,
                 modifier = Modifier.padding(all = 4.dp),
+                color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.body2
             )
         }
     }
 }
 
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
 
 
-@Preview
+@Preview(name = "Dark Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Light Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     ComposeBeginsTheme {
-        MessageCard(
-            msg = Message(
-                "Android",
-                "Hey... Jetpack Compose, it's great!")
-        )
+        Conversation(SampleData.conversationSample)
     }
 }
+
 
