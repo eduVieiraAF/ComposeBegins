d0f4c939ef34fcda04460b5f7c74254143c5eb8e
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-26 17:10:02 -0300

    Animation and Lists
    LazyColumn working as a RecyclerView && Animation

--- app/src/main/java/com/example/composebegins/jc_codelab1/MainActivity.kt
+++ app/src/main/java/com/example/composebegins/jc_codelab1/MainActivity.kt
@@ -4,18 +4,20 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.estimateAnimationDurationMillis
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composebegins.ui.theme.ComposeBeginsTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,21 +32,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
     } else {
         Greetings()
     }
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
     Surface(color = MaterialTheme.colors.background) {
         Column(modifier = Modifier.padding(14.dp)) {
-            for (name in names){
-                Greeting(name)
+            LazyColumn {
+                items(names) { name -> Greeting(name) }
             }
         }
     }
@@ -53,19 +55,24 @@ fun Greetings(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false) }
-    val extraPadding = if (expanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 72.dp else 0.dp,
+        animationSpec = tween(durationMillis = 1500)
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)) {
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
                     .padding(bottom = extraPadding)
             ) {
-                Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = "LazyColumn\n(instead of RecyclerView)")
+                Text(text = " → Slot #$name")
+
             }
 
             OutlinedButton(onClick = { expanded = !expanded }) {
@@ -76,7 +83,7 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
+fun OnBoardingScreen(onContinueClicked: () -> Unit) {
     // TODO: This state should be hoisted
 
     Surface {
@@ -85,20 +92,28 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to the Basics Codelab!")
+            Text("Codelab #1 COMPLETE!")
 
             Button(
                 modifier = Modifier.padding(vertical = 21.dp),
                 onClick = onContinueClicked
             ) {
-                Text("Continue")
+                Text("Continue to see")
             }
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true)
+@Composable
+fun ShowMyAPP() {
+    ComposeBeginsTheme {
+        MyApp()
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun ShowGreetings() {
     ComposeBeginsTheme {
@@ -106,13 +121,12 @@ fun ShowGreetings() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ShowOnBoarding() {
     ComposeBeginsTheme {
-        MyApp()
+        OnBoardingScreen {
+
+        }
     }
 }
-
-
